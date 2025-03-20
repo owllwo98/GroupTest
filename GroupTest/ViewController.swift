6f0169e61aaf2b1ef4d05d54bd8bb5751c4a7068
owllwo98 <sloopy98@naver.com>
2025-03-20 15:11:48 +0900

    Initial Commit

---
+++ GroupTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroupTest
+//
+//  Created by 변정훈 on 3/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
